b501604bd72842f6c25efd31bc811a428cb66d2e
Eliza Juodvalkyte <eliza@paytack.dk>
2023-05-17 11:09:51 +0300

    add register form

--- src/UI/Register.jsx
+++ src/UI/Register.jsx
@@ -5,20 +5,21 @@ const Register = () => {
         <h2>Don&apos;t have an account? Register Here</h2>
         <div>
           <label htmlFor="name">Name</label>
-
+          <input type="text" id="name" name="name" />
         </div>
         <div>
           <label htmlFor="email">Email</label>
-
+          <input type="email" id="email" name="email" />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-
+          <input type="password" id="password" name="password" />
         </div>
         <div>
-          <label htmlFor="password_repeat">Repeat password</label>
-
+          <label htmlFor="passwordConfirm">Repeat password</label>
+          <input type="password" id="passwordConfirm" name="passwordConfirm" />
         </div>
+        <input type="submit" value="Register" />
       </form>
     </>
   )
