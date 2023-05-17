892c983a9ba900f38e219e948aef283d4724acb9
Eliza Juodvalkyte <eliza@paytack.dk>
2023-05-17 12:16:39 +0300

    add formik and yup validation

--- src/UI/Register.jsx
+++ src/UI/Register.jsx
@@ -1,23 +1,99 @@
+import { useContext, useState } from "react"
+import UsersContext from "../contexts/UsersContext"
+import * as Yup from 'yup'
+import { useFormik } from "formik"
+
 const Register = () => {
+  const [failedRegistration, setFailedRegistration] = useState(false)
+  const { users, dispatch, setCurrentUser } = useContext(UsersContext)
+
+  const values = {
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  }
+
+  const ErrorMessages = {
+    EMAIL_VALIDATION: 'This input must be a valid email',
+    REQUIRE: 'Input must be filled',
+    MIN: 'Input must be at least',
+    MATCH: 'Passwords must match'
+  }
+
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .required(ErrorMessages.REQUIRE),
+    email: Yup.string()
+      .email(ErrorMessages.EMAIL_VALIDATION)
+      .required(ErrorMessages.REQUIRE),
+    password: Yup.string()
+      .min(5, ErrorMessages.MIN + ' 5 symbols long')
+      .required(ErrorMessages.REQUIRE),
+    passwordConfirm: Yup.mixed()
+      .oneOf([Yup.ref('password')], ErrorMessages.MATCH)
+      .required(ErrorMessages.REQUIRE),
+  })
+
+  const formik = useFormik({
+    initialValues: values,
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      console.log(values)
+    }
+  })
+
   return (
     <>
-      <form onSubmit="">
-        <h2>Don&apos;t have an account? Register Here</h2>
+      <form onSubmit={formik.handleSubmit}>
+        <h2>Don&apos;t have an account? Register </h2>
         <div>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" />
+          <input type="text" id="name" name="name"
+            value={formik.values.name}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {
+            formik.touched.name && formik.errors.name &&
+            <span>{formik.errors.name}</span>
+          }
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
+          <input type="email" id="email" name="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {
+            formik.touched.email && formik.errors.email &&
+            <span>{formik.errors.email}</span>
+          }
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" />
+          <input type="password" id="password" name="password"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {
+            formik.touched.password && formik.errors.password &&
+            <span>{formik.errors.password}</span>
+          }
         </div>
         <div>
           <label htmlFor="passwordConfirm">Repeat password</label>
-          <input type="password" id="passwordConfirm" name="passwordConfirm" />
+          <input type="password" id="passwordConfirm" name="passwordConfirm"
+            value={formik.values.passwordConfirm}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+          />
+          {
+            formik.touched.passwordConfirm && formik.errors.passwordConfirm &&
+            <span>{formik.errors.passwordConfirm}</span>
+          }
         </div>
         <input type="submit" value="Register" />
       </form>
