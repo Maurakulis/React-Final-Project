5317efa39ceda2e13659880091ad8b2699e6d476
Eliza Juodvalkyte <eliza@paytack.dk>
2023-05-18 10:53:27 +0300

    display current user in header

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react"
 import { Link, NavLink } from "react-router-dom"
+import UsersContext from "../contexts/UsersContext"
 
 const Header = () => {
+  const { currentUser, setCurrentUser } = useContext(UsersContext)
+
   return (
     <header>
       <nav>
@@ -10,8 +14,27 @@ const Header = () => {
           </Link>
         </div>
         <ul>
-          <li><NavLink to='/'>Sign In</NavLink></li>
+          {
+            !currentUser ?
+              <li><NavLink to='/'>Sign In</NavLink></li> :
+              <li onClick={() => {
+                setCurrentUser(null)
+              }}><a>Log Out</a></li>
+          }
           <li><NavLink to='/allQuestions'>All questions</NavLink></li>
+          <li>
+            {
+              currentUser &&
+              <div>
+                {currentUser.imageUrl ?
+                  <img src={currentUser.imageUrl} alt="" /> :
+                  <div className="image">
+                    <span>{currentUser.name.slice(0, 1)}</span>
+                  </div>
+                }
+              </div>
+            }
+          </li>
         </ul>
       </nav>
     </header>
