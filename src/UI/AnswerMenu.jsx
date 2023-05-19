87639f9111bcf7ff1b9cebdce829be86ef091839
Eliza Juodvalkyte <eliza@paytack.dk>
2023-05-19 11:34:28 +0300

    style answers menu

--- src/UI/AnswerMenu.jsx
+++ src/UI/AnswerMenu.jsx
@@ -1,9 +1,33 @@
+import { useContext, useState } from "react"
+import AnswersContext from "../contexts/AnswersContext"
+
 const AnswerMenu = () => {
+  const [menuIsPressed, setMenuIsPressed] = useState(false)
+  const { dispatch } = useContext(AnswersContext)
+
+  const editAnswer = () => {
+    console.log('edited')
+    setMenuIsPressed(!menuIsPressed)
+
+  }
+
+  const deleteAnswer = () => {
+    console.log('deleted')
+    setMenuIsPressed(!menuIsPressed)
+
+  }
 
   return (
     <>
-      <i className="fa-solid fa-ellipsis" onClick={() => { }}></i>
-
+      <i className="fa-solid fa-ellipsis" onClick={() => setMenuIsPressed(!menuIsPressed)}></i>
+      {
+        menuIsPressed &&
+        <div className="answerMenu">
+          <button onClick={() => editAnswer()}>Edit</button>
+          <div></div>
+          <button onClick={() => deleteAnswer()}>Delete</button>
+        </div>
+      }
     </>
   )
 }
