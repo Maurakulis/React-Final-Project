814ca966c12eafa46cf0e11f590cd4012f475a85
Eliza Juodvalkyte <eliza@paytack.dk>
2023-05-22 17:13:39 +0300

    style sort buttons

--- src/Pages/AllQuestions.jsx
+++ src/Pages/AllQuestions.jsx
@@ -1,12 +1,14 @@
 import { useContext } from "react"
 import QuestionsContext from "../contexts/QuestionsContext"
 import Question from "../UI/Question"
+import Sort from "../UI/Sort"
 
 const AllQuestions = () => {
   const { questions } = useContext(QuestionsContext)
 
   return (
-    <main>
+    <main className="allQuestions">
+      <Sort />
       {
         Array.isArray(questions) && questions.length > 0 &&
         <section className="allQuestions">
---
+++ src/UI/Sort.jsx
@@ -0,0 +1,11 @@
+const Sort = () => {
+  return (
+    <section className="sorting">
+      <button>Newest</button>
+      <button>Oldest</button>
+      <button>All</button>
+    </section>
+  )
+}
+
+export default Sort
