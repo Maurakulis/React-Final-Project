6c2c4c930ae40c5892e0cb1b0ff1bd0d3ff2a540
Eliza Juodvalkyte <eliza@paytack.dk>
2023-05-23 15:49:32 +0300

    add form to EditAnswer

--- src/Pages/EditAnswer.jsx
+++ src/Pages/EditAnswer.jsx
@@ -1,7 +1,20 @@
+import { useContext, useState } from "react"
+import { useParams } from "react-router-dom"
+import AnswersContext from "../contexts/AnswersContext"
+
 const EditAnswer = () => {
+  const { id } = useParams()
+  const { dispatch } = useContext(AnswersContext)
+  const [answer, setAnswer] = useState()
+
   return (
-    <>
-      answer</>
+    <main>
+      <form>
+        <textarea name="text" id="text" />
+        <input type="submit" value="Save changes" />
+
+      </form>
+    </main>
   )
 }
 
